31af094dbca06a16a848a18d80a7cfdd57bcfc7c
Ryota Higashi <ryo19870130@outlook.jp>
2022-03-26 23:57:34 +0900

    画面遷移

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'next_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -50,8 +52,11 @@ class _MyHomePageState extends State<MyHomePage> {
             return Column(
                 children:[
                   ListTile(
-                  leading: Icon(Icons.vpn_key),
-                  title: Text(titleList[i])
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => NextPage()));
+                    },
+                    leading: Icon(Icons.vpn_key),
+                    title: Text(titleList[i])
                   ),
                   Divider(),
                 ]
